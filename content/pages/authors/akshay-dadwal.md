fbdb40ecb6f9df5ea251f3daaa9bad675605e3d5
dadwalakshay <akshayd00@outlook.com>
2022-08-19 16:08:08 +0530

    Initializing static blog

---
+++ content/pages/authors/akshay-dadwal.md
@@ -0,0 +1,12 @@
+title: About
+slug: authors/akshay-dadwal
+tags: author
+status: published
+
+---
+Hello World!  
+It's ***Akshay Dadwal***, I am a Backend Engineer with 4+ years of professional experience.
+
+I have been building Web Applications with Python and Node.js, and tinkering stuff with Elixir.
+
+I will be publishing blogs about Python and Django or some other interesting findings, ***stay tuned...***
---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+AUTHOR = "Akshay Dadwal <akshayd00@outlook.com>"
+SITENAME = "Blog"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "Asia/Calcutta"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = False
+
+DELETE_OUTPUT_DIRECTORY = True
+
+ARTICLE_URL = "blog/{slug}/"
+ARTICLE_SAVE_AS = "blog/{slug}.html"
+
+PAGE_URL = "pages/{slug}/"
+PAGE_SAVE_AS = "pages/{slug}.html"
+
+CATEGORIES_URL = "category/"
+CATEGORY_URL = "category/{slug}/"
+CATEGORY_SAVE_AS = "category/{slug}.html"
+
+TAG_URL = "tag/{slug}/"
+TAG_SAVE_AS = "tag/{slug}.html"
+
+AUTHOR_URL = "pages/authors/{slug}/"
