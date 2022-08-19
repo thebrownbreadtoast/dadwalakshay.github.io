cfd7d2f75448d134f151a7228cab054002dc35b0
dadwalakshay <akshayd00@outlook.com>
2022-08-20 00:20:12 +0530

    absolute *.html paths configured and static-site moved to docs/

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,7 @@
 AUTHOR = "Akshay Dadwal <akshayd00@outlook.com>"
 SITENAME = "blog"
 SITESUBTITLE = "A low effort blog"
-SITEURL = ""
+# SITEURL = "https://dadwalakshay.github.io"
 
 PATH = "content"
 
@@ -18,28 +18,28 @@ AUTHOR_FEED_RSS = None
 
 DEFAULT_PAGINATION = 5
 
-LINKS = (("Blog", "/category/blog/"), ("Resume", "/#"))
+LINKS = (("Blog", "category/blog.html"), ("Resume", "#"))
 
 FOOTER_LINKS = (("", "/#"),)
 
 HIDE_AUTHORS = False
-AUTHOR_URL = "pages/authors/{slug}/"
+AUTHOR_URL = "pages/authors/{slug}.html"
 
 SITEIMAGE = "images/profile_picture.jpeg width=200px height=200px"
 
 DELETE_OUTPUT_DIRECTORY = True
 
-ARTICLE_URL = "blog/{slug}/"
+ARTICLE_URL = "blog/{slug}.html"
 ARTICLE_SAVE_AS = "blog/{slug}.html"
 
-PAGE_URL = "pages/{slug}/"
+PAGE_URL = "pages/{slug}.html"
 PAGE_SAVE_AS = "pages/{slug}.html"
 
-CATEGORIES_URL = "category/"
-CATEGORY_URL = "category/{slug}/"
+CATEGORIES_URL = "category/blog.html"
+CATEGORY_URL = "category/{slug}.html"
 CATEGORY_SAVE_AS = "category/{slug}.html"
 
-TAG_URL = "tag/{slug}/"
+TAG_URL = "tag/{slug}.html"
 TAG_SAVE_AS = "tag/{slug}.html"
 
 THEME = "themes/pelican-alchemy/alchemy"
@@ -52,4 +52,4 @@ STATIC_PATHS = [
 ]
 
 # Generating static-site into docs/ to serve it using github-pages
-OUTPUT_PATH = "."
+OUTPUT_PATH = "docs/"
